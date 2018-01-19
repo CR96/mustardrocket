d25232fe5e2ea590fc15f95a15563136b8aadb7e
Corey Rowe <coreyr1196@gmail.com>
2018-01-19 16:58:58 -0500

    Retrieve json

--- app/src/main/java/com/corey/mustardrocket/activity/MainActivity.java
+++ app/src/main/java/com/corey/mustardrocket/activity/MainActivity.java
@@ -1,7 +1,22 @@
-package com.corey.mustardrocket;
+package com.corey.mustardrocket.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.corey.mustardrocket.R;
+import com.corey.mustardrocket.model.Liveries;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Deserialize livery feed
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://104.131.89.66:81/api/liveries.json";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Gson gson = new GsonBuilder()
+                        .create();
+                Liveries liveriesJson = gson
+                        .fromJson(response.toString(), Liveries.class);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+            }
+        }
+        );
+        queue.add(jsonObjectRequest);
     }
 }
